75730f813d270ac49950dbf14b4c9ab5db531afa
kishiro143 <pulguitacdr14@gmail.com>
2026-03-13 11:39:50 -0500

    Update test_funciones.py

--- tests/test_funciones.py
+++ tests/test_funciones.py
@@ -18,8 +18,8 @@ class TestMaximaNota(unittest.TestCase):
         self.assertIsNone(maxima_nota(notas))
 
     def test_maxima_nota_todas_cero(self):
-        notas = [0.0, 0.0, 0.0]
-        self.assertEqual(maxima_nota(notas), 0.0)
+        notas = [0.0, 3.5, 0.0]
+        self.assertEqual(maxima_nota(notas), 3.5)
 
 if __name__ == '__main__':
     unittest.main()
